d59ec3e56c210453eb8c3cec9b3e8e3c38131c9b
BhargavaRamM <bhargava@udel.edu>
2015-11-20 22:45:47 -0500

    Affine Parameters

--- AffineP.m
+++ AffineP.m
@@ -36,28 +36,7 @@ XT*AT==BT;
 Y=inv(X*XT);
 Z=XT*Y;
 final1 = B*Z
-%out = final1;
+out = final1;
 
-C = final1*[1 1 c2 c2;1 r2 r2 1;1 1 1 1];
-Xpr = min([C(1,:) 0]) : max([C(1,:) c1]);
-Ypr = min([C(2,:) 0]) : max([C(2,:) r1]);
-[XP,YP] = ndgrid(Xpr,Ypr);
-[cp rp] = size(XP);
-
-X = final1 \ [ XP(:) YP(:) ones(cp*rp,1) ]';  % warp
-
-%clear Ip;
-xI = reshape( X(1,:),cp,rp)';
-yI = reshape( X(2,:),cp,rp)';
-Ip(:,:,1) = interp2(I2(:,:,1), xI, yI, '*bilinear'); % red
-Ip(:,:,2) = interp2(I2(:,:,2), xI, yI, '*bilinear'); % green
-Ip(:,:,3) = interp2(I2(:,:,3), xI, yI, '*bilinear'); % blue
-
-% offset and copy original image into the warped image
-offset =  -round( [ min( [ C(1,:) 0 ] ) min( [ C(2,:) 0 ] ) ] );
-Ip(1+offset(2):r1+offset(2),1+offset(1):c1+offset(1),:) = double(I1(1:r1,1:c1,:));
-
-% show the result
-out=Ip;
 
 end
